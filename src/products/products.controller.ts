db1ae0cb02aabd4a03101aab533f46a93d5d0a41
Javad Ghasemi <javad.ghasemi8080@gmail.com>
2024-03-29 01:00:33 +0330

    write create api for products module

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateProductDto } from './dto/CreateProduct.dto';
+import { ProductsService } from './products.service';
 
 @Controller({
   path: 'products',
   version: '1',
 })
 export class ProductsController {
+  constructor(private productsService: ProductsService) {}
   @Post()
-  public create(@Body() product: CreateProductDto) {}
+  public create(@Body() product: CreateProductDto) {
+    return this.productsService.create();
+  }
 }
